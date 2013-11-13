74e30370ce883bb0f0ffcce86acb71b63a364fd9
Bartosz Wozniak <bdw10@imperial.ac.uk>
2013-11-13 19:40:49 +0000

    Added the test folder and an initial test.

---
+++ test/com/acmetelecom/BillGeneratorTest.java
@@ -0,0 +1,17 @@
+package com.acmetelecom;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class BillGeneratorTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
