5fbb61a3568ea89cb0abd78e63acad704850857f
mp3510 <maxwellpopescu@gmail.com>
2013-11-14 16:54:38 +0000

    changed to use jodatime

--- src/com/acmetelecom/Call.java
+++ src/com/acmetelecom/Call.java
@@ -1,39 +1,40 @@
 package com.acmetelecom;
 
-import javax.swing.text.DateFormatter;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class Call {
-    private CallEvent start;
-    private CallEvent end;
+    private final CallEvent start;
+    private final CallEvent end;
 
-    public Call(CallEvent start, CallEvent end) {
-        this.start = start;
-        this.end = end;
+    public Call(final CallEvent start, final CallEvent end) {
+	this.start = start;
+	this.end = end;
     }
 
     public String callee() {
-        return start.getCallee();
+	return start.getCallee();
     }
 
     public int durationSeconds() {
-        return (int) (((end.time() - start.time()) / 1000));
+	return (int) (((end.time() - start.time()) / 1000));
     }
 
     public String date() {
-        return SimpleDateFormat.getInstance().format(new Date(start.time()));
+	// return SimpleDateFormat.getInstance().format(new Date(start.time()));
+
+	// Returns identical string to java.date as before
+	final DateTimeFormatter dtf = DateTimeFormat.shortDateTime();
+	return new DateTime(System.currentTimeMillis()).toString(dtf);
+
     }
 
-    public Date startTime() {
-        return new Date(start.time());
+    public DateTime startTime() {
+	return new DateTime(start.time());
     }
 
-    public Date endTime() {
-        return new Date(end.time());
+    public DateTime endTime() {
+	return new DateTime(end.time());
     }
 }
