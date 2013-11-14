1b7143b5888a43940e38cd6b5de84a7f49ee8075
Bartosz Wozniak <bdw10@ic.ac.uk>
2013-11-14 11:36:28 +0000

    Extracted hours for start and end of the peak period out of the method and made them constructor arguments.

--- src/com/acmetelecom/DaytimePeakPeriod.java
+++ src/com/acmetelecom/DaytimePeakPeriod.java
@@ -5,10 +5,26 @@ import java.util.Date;
 
 class DaytimePeakPeriod {
 
-    public boolean offPeak(Date time) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(time);
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        return hour < 7 || hour >= 19;
+    private final int peakStart;
+    private final int peakEnd;
+
+    public DaytimePeakPeriod(final int peakStart, final int peakEnd) {
+	super();
+	this.peakStart = peakStart;
+	this.peakEnd = peakEnd;
+    }
+
+    @Deprecated
+    public DaytimePeakPeriod() {
+	super();
+	peakStart = 7;
+	peakEnd = 19;
+    }
+
+    public boolean offPeak(final Date time) {
+	final Calendar calendar = Calendar.getInstance();
+	calendar.setTime(time);
+	final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+	return hour < peakStart || hour >= peakEnd;
     }
 }
