5ae7a1251ef9dedacd76f774a27a4092ccc4bd33
Bartosz Wozniak <bdw10@ic.ac.uk>
2013-11-14 13:33:52 +0000

    Implemented a test of the offPeak method for different timezones.

--- test/com/acmetelecom/DaytimePeakPeriodTest.java
+++ test/com/acmetelecom/DaytimePeakPeriodTest.java
@@ -26,15 +26,13 @@ public class DaytimePeakPeriodTest {
 	final DaytimePeakPeriod period = new DaytimePeakPeriod(7, 19);
 
 	final String[] zones = { "UTC", "WET", "PST", "GMT", "EST", "ECT" };
-	// Change the default timezone and create new dates
+	// Change the default timezone and test the dates
 	for (final String zone : zones) {
 	    TimeZone.setDefault(TimeZone.getTimeZone(zone));
 
-	    final Date timePeak = new Date(2013, 11, 12, 8, 10, 10);
-	    final Date timeOffPeakEarly = new Date(2013, 11, 12, 6, 10, 10);
-	    final Date timeOffPeakLate = new Date(2013, 11, 12, 20, 10, 10);
-
-	    System.out.println(timePeak.toString());
+	    final Date timePeak = new Date(2013, 11, 12, 8, 10, 10); // Inside the peak
+	    final Date timeOffPeakEarly = new Date(2013, 11, 12, 6, 10, 10); // Before peak
+	    final Date timeOffPeakLate = new Date(2013, 11, 12, 20, 10, 10); // After peak
 
 	    assertFalse(period.offPeak(timePeak));
 	    assertTrue(period.offPeak(timeOffPeakEarly));
